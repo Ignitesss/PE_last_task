d6be665f4cf5198040d9dd1ccba862f806f67851
Yana <58274732+Ignitesss@users.noreply.github.com>
2022-12-26 01:30:28 +0300

    Added new files

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+
+
+namespace ConsoleApp1
+{
+    public class Function
+    {
+        public Function()
+        { }
+
+        public BigInteger Factorial(int n)
+        {
+            var factorial = new BigInteger(1);
+            for (int i = 1; i <= n; i++)
+                factorial *= i;
+
+            return factorial;
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var tmp = new Function();
+            Console.WriteLine(tmp.Factorial(5));
+        }
+    }
+}
