69f3a08bddd0a175e64433b0820039d9901e3a70
Yana <58274732+Ignitesss@users.noreply.github.com>
2022-12-26 00:23:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static public BigInteger Factorial(int n)
+        {
+            var factorial = new BigInteger(1);
+            for (int i = 1; i <= n; i++)
+                factorial *= i;
+
+            return factorial;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(5));
+        }
+    }
+}
